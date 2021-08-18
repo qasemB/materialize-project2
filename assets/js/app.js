ccbb3e3d9e9369dee5a966a376cd4ff6efb91ea6
qasemB <mahdicmptr@gmail.com>
2021-08-19 00:26:58 +0430

    part #21

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,4 +3,16 @@ document.addEventListener('DOMContentLoaded', function() {
     M.Sidenav.init(elems, {
         edge : 'right',
     });
+
+    var carouselElems = document.querySelectorAll('.carousel');
+    M.Carousel.init(carouselElems, {
+        fullWidth: true,
+        indicators: true,
+    });
+
+    setInterval(function(){
+        M.Carousel.getInstance(carouselElems[0]).next()
+    } , 5000)
+
+
 });
