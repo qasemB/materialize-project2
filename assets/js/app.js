7481f47d73c9a57d4e538c24745b038cb070ce85
qasemB <mahdicmptr@gmail.com>
2021-09-17 00:03:01 +0430

    part #36

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
         edge : 'right',
     });
 
-    var carouselElems = document.querySelectorAll('.carousel');
+    var carouselElems = document.querySelectorAll('.carousel.top_carousel');
     M.Carousel.init(carouselElems, {
         fullWidth: true,
         indicators: true,
@@ -18,6 +18,20 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         },
     });
+
+    var galleryItems = document.querySelectorAll('.carousel.gallery_div');
+    M.Carousel.init(galleryItems, {});
+
+    var simpleItem = document.querySelectorAll('.simple_item');
+    for (const item of simpleItem) {
+        item.addEventListener('click' , function(){
+            document.getElementById('gallery_section').classList.add('active');
+        })
+    }
+
+    document.getElementById('close_gallery_section').addEventListener('click' , function(){
+        document.getElementById('gallery_section').classList.remove('active');
+    })
 
     setInterval(function(){
         M.Carousel.getInstance(carouselElems[0]).next()
